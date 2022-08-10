a6d3cc655a89d7bc84dabe7ce44571616087a4f5
jdk2531 <jdk2531@gmail.com>
2022-08-10 22:36:21 +0900

    update

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
+    # admin
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+
+    # project app
+    path('', base_views.index, name='index'),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
 ]
--- pybo/models.py
+++ pybo/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
@@ -13,7 +15,13 @@ class Question(models.Model):
 class Answer(models.Model):
     # ForeignKey : 다른 모델과 연결
     # on_delete=models.CASCADE : 질문이 삭제될 경우, 답변도 함께 삭제됨
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    # null = True : 날짜 수정에 null 허용
+    # blank = True : 입력 데이터 검증 시 값이 없어도 됨
+    # => 어떤 조건으로든 값을 비워둘 수 있음을 의미
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
+
